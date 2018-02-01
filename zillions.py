a1095dab24d3ce940beb3f288f710e9a54754256
ytrepus <rsuperty@yahoo.com>
2018-02-01 08:41:51 +0000

    initial version

--- zillions.py
+++ zillions.py
@@ -19,9 +19,33 @@
     https://en.wikipedia.org/wiki/Long_and_short_scales
 """
 
+from itertools import chain, repeat
 from math import log10, trunc
+import re
+from typing import List
 
 MAX_VALUE = 10**66 - 1
+
+ONES = [
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+    ]
+TEENS = [
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 
+    'seventeen', 'eighteen', 'nineteen'
+    ]
+TENS = [
+    '', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 
+    'eighty', 'ninety'
+    ]
+ZILLIONS = [
+    '',  'thousand', 'million', 'billion', 'trillion', 'quadrillion', 
+    'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 
+    'decillion', 'undecillion', 'duodecillion', 'tredecillion', 
+    'quattuordecillion', 'quindecillion', 'sexdecillion', 'septendecillion', 
+    'octodecillion', 'novemdecillion', 'vigintillion'
+    ]
+
+ZILLIARDS = [z.replace('ion', 'iard') for z in ZILLIONS[2:]]
 
 def num_digits(num: int) -> int:
     """Return the number of digits of an integer."""
@@ -40,18 +64,53 @@ def name_int(num: int, scale='short') -> str:
     sign = 'negative ' if num < 0 else ''
     num = abs(num)
 
-    period_len = 3 if scale == 'short' else 6
-    init_period_len = num_digits(num) % period_len
-    if init_period_len == 0:
-        init_period_len == period_len
-    pass
-    
+    periods = get_periods(num)
+    period_names = map(name_period, periods)
+    zillions = get_zillions(scale)
+    name_list = []
+    for period_name, zillion in zip(period_names, zillions):
+        if not period_name:
+            continue
+        name_segment = f"{period_name} {zillion}"
+        name_list.append(name_segment)
+    return re.sub(r' $', '', ' '.join(reversed(name_list)))
 
-# dictionary: sexdecillion
-# henckle: sexdecillion
-# natural: sexdecillion
-# zilli: sexdecillion
-# conway: sedecillion
-# conway_modified: sedecillion
-# conway_simplified: sexdecillion
-# conway_reversed: sexdecillion
+
+def get_periods(num: int) -> List[str]:
+    digits = str(num)
+    start = -3
+    period = digits[start:]
+    period_list = []
+    while period:
+        period_list.append(period)
+        start, end = start - 3, start
+        period = digits[start: end]
+    return period_list
+
+
+def name_period(period: str) -> str:
+
+    period = int(period)
+    h_digit = period // 100
+    t_digit = (period % 100) // 10
+    o_digit = period % 10
+    h_name = ONES[h_digit] + " hundred " if h_digit else ""
+    
+    if t_digit == 0:
+        to_name = ONES[o_digit]
+    elif t_digit == 1:
+        to_name = TEENS[o_digit]
+    else:
+        to_name = TENS[t_digit] + '-' + ONES[o_digit]
+
+    name = h_name + to_name
+    return re.sub(r' $', '', name)
+
+def get_zillions(scale):
+    if scale == 'long_eu':
+        return ZILLIONS[:2] + list(chain(*zip(ZILLIONS[2:], ZILLIARDS)))
+    elif scale == 'long_br':
+        return ZILLIONS[:2] + list(chain(*zip(ZILLIONS[2:],
+                                   repeat("thousand"))))
+    else:
+        return ZILLIONS  
