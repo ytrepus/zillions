6aeede0b6e680cf8331410bfd7843089ee74cc7c
ytrepus <rsuperty@yahoo.com>
2018-02-13 19:26:54 +0000

    extension ideas

--- more_zillions.py
+++ more_zillions.py
@@ -1,2 +1,24 @@
-# other forms of zillions
-    # 
+"""This module will extend some of the functionality of the original zillions
+module, including some or all of:
+
+long scales (British-style and European-style)
+Knuth -yillions
+
+localization options (British 'one hundred and one' vs. American 
+    'one hundred one')
+
+other systems of naming zillions
+    natural: vigintillion-trillion, vigintillion-quintillion
+    conway: tresvigintillion, quinquavigintillion
+        modified conway: tresvigintillion, quinvigintillion
+        simplified conway: trevigintillion, quinvigintillion
+        reversed conway: vigintitrillion, vigintiquintillion
+    zilli: trilli-vigintillion, quintilli-vigintillion
+    latin: tertio-vigintillion, quinto-vigintillion
+
+extend to millillion (10^3003) or indefinitely
+
+words to numbers
+
+naming fractions and decimals
+"""
